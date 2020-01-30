abf7fd193acb026c7a9363a2b40b06dde7a89674
MacBook <macbook@MacBook-Pro-MacBook.local>
2020-01-30 21:02:46 +0300

    feature: adds filter

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,27 +1,95 @@
-(function () {
-  'use strict';
+'use strict';
 
-  function trackScroll() {
-    var scrolled = window.pageYOffset;
-    var coords = document.documentElement.clientHeight;
+function trackScroll() {
+  let scrolled = window.pageYOffset;
+  let coords = document.documentElement.clientHeight;
 
-    if (scrolled > coords) {
-      goTopBtn.classList.add('to-top--show');
-    }
-    if (scrolled < coords) {
-      goTopBtn.classList.remove('to-top--show');
+  if (scrolled > coords) {
+    goTopBtn.classList.add('to-top--show');
+  }
+  if (scrolled < coords) {
+    goTopBtn.classList.remove('to-top--show');
+  }
+}
+
+function backToTop() {
+  if (window.pageYOffset > 0) {
+    window.scrollBy(0, -80);
+    setTimeout(backToTop, 0);
+  }
+}
+
+let goTopBtn = document.querySelector('.to-top');
+
+window.addEventListener('scroll', trackScroll);
+goTopBtn.addEventListener('click', backToTop);
+//  scroll Top
+
+document.querySelector('#sortPrice').onclick = function () {
+  this.classList.toggle('active');
+}
+
+document.querySelector('#sortAmount').onclick = function () {
+  this.classList.toggle('active');
+}
+
+document.querySelector('.catalog__sort-btn--asc').onclick = function () {
+  sortList('data-price');
+}
+document.querySelector('.catalog__sort-btn--desc').onclick = function () {
+  sortListDesc('data-price');
+}
+document.querySelector('.catalog__sort-btn--amount-asc').onclick = function () {
+  sortListDesc('data-amount');
+}
+document.querySelector('.catalog__sort-btn--amount-desc').onclick = function () {
+  sortList('data-amount');
+}
+
+function sortList(sortType) {
+  let items = document.querySelector('.catalog__list');
+  for (let i = 0; i < items.children.length - 1; i++) {
+    for (let j = i; j < items.children.length; j++) {
+      if (+items.children[i].getAttribute(sortType) > +items.children[j].getAttribute(sortType)) {
+        console.log(1);
+        let replacedNode = items.replaceChild(items.children[j], items.children[i]);
+        insertAfter(replacedNode, items.children[i]);
+      }
     }
   }
+}
 
-  function backToTop() {
-    if (window.pageYOffset > 0) {
-      window.scrollBy(0, -80);
-      setTimeout(backToTop, 0);
+function sortListDesc(sortType) {
+  let items = document.querySelector('.catalog__list');
+  for (let i = 0; i < items.children.length - 1; i++) {
+    for (let j = i; j < items.children.length; j++) {
+      if (+items.children[i].getAttribute(sortType) < +items.children[j].getAttribute(sortType)) {
+        console.log(1);
+        let replacedNode = items.replaceChild(items.children[j], items.children[i]);
+        insertAfter(replacedNode, items.children[i]);
+      }
     }
   }
+}
 
-  var goTopBtn = document.querySelector('.to-top');
 
-  window.addEventListener('scroll', trackScroll);
-  goTopBtn.addEventListener('click', backToTop);
-})();
+function insertAfter(elem, refElem) {
+  return refElem.parentNode.insertBefore(elem, refElem.nextSibling);
+}
+
+//  Sort
+
+
+function validate(subscribe, email) {
+  let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  let address = document.forms[subscribe].elements[email].value;
+  if (reg.test(address) == false) {
+    alert('Введите корректный e-mail');
+    return false;
+  } else {
+    alert('Вы успешно подписались');
+    validate.preventDefault();
+  }
+}
+
+//  Validation
